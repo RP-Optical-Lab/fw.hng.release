98b5c834c0d14b85b7c16f9908003e96284ae92e
Max Parker <max@rp>
2024-08-22 00:43:29 -0700

    v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# fw.hng.release
-FW HNG Project
+# FW HNG Project
+
+## How to update
+
+```
+chmod +x update.sh 
+sudo ./update.sh fw_hng-xxx.tar.gz
+```
+
+# What's new in the latest version
+
+
+## Version fw_hng-001
+### New:
+1. All new ... ⚡
+### Bugs fixed:
+1. New release without bugs and problems 🏆
+### Notes:
+1. The release works as expected
---
+++ update.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+prints () {
+	echo "# "$@
+}
+
+usage () {
+	prints usage:   ./update.sh file
+	prints example: ./update.sh fw_hng-xxx.tar.gz
+	exit 1
+}
+
+action () {
+	prints $@
+	eval $@
+	if [ $? -ne 0 ]; then
+		prints error: Command failed!
+		usage
+	fi
+}
+
+prints "#####################################################################"
+
+if [ "$#" -ne 1 ]; then
+    prints error: Provide version
+    usage
+fi
+
+FILE="$1"
+FW="$(basename "$FILE" .tar.gz)"
+
+# if pgrep -x "hng" > /dev/null; then
+#     action sudo killall hng
+# fi
+
+prints Setting $FW
+action sudo tar xvf $FILE -C /lib/firmware/xilinx
+action 'echo '$FW' | sudo tee /etc/dfx-mgrd/'$FW''
+action sudo ln -sf /etc/dfx-mgrd/$FW /etc/dfx-mgrd/default_firmware
+action ls -l /etc/dfx-mgrd
+action more /etc/dfx-mgrd/$FW
+prints Done!
+prints Restart the system!
+
+
+# RM FW
+# sudo rm -rf /lib/firmware/FW* /lib/firmware/xilinx/FW /etc/dfx-mgrd/FW
